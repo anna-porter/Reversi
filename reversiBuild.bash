f3d837c1c6b322f5e040a4593b05b219b06ee63a
asiansofshield <anna.porter202@gmail.com>
2018-07-13 16:52:53 -0500

    Update reversiBuild.bash
    gutted anything that was funcs.cpp

--- reversiBuild.bash
+++ reversiBuild.bash
@@ -21,82 +21,6 @@ then
    exit 1
 fi
 
-#for fileName in ${challenge}Agent?*.cpp
-#do
-#   funcName="${fileName%.cpp}"
-#   agentName="${funcName#${challenge}Agent}"
-#   validAgentNameFormat='^[A-Z][0-9A-Za-z]*$'
-#   if [[ "${#agentName}" -le 20 && "${agentName}" =~ ${validAgentNameFormat} ]]
-#   then
-#      if ! grep "#include \"${challenge}.h\"" "${fileName}" > /dev/null
-#      then
-#         echo " * ${fileName}: No #include \"${challenge}.h\" found; skipping compilation."
-#      elif ! grep -w "${funcName}" "${fileName}" > /dev/null
-#      then
-#         echo " * ${fileName}: No ${funcName} function found; skipping compilation."
-#      else
-#         echo " * ${fileName}: Compiling . . ."
-#         if ${CXX} ${CXXFLAGS} -c "${fileName}"
-#         then
-#            echo " * ${fileName}: Compilation succeeded."
-#            funcNames="${funcNames} ${funcName}"
-#            objectFiles="${objectFiles} ${funcName}.o"
-#            (( numAgents += 1 ))
-#         else
-#            echo " * ${fileName}: Compilation failed; skipping agent ${agentName}."
-#         fi
-#      fi
-#   else
-#     echo " * ${fileName}: Invalid agent name; skipping compilation."
-#   fi
-#done
-
-#echo " * ${numAgents} agents found."
-#if (( numAgents < 1 ))
-#then
-#   echo " * At least one agent (e.g., ${challenge}AgentSmith.cpp) is needed."
-#   echo " * Build failed."
-#   rm *.o
-#   exit 2
-#fi
-
-cat >| "${challenge}Funcs.cpp" << END
-#include "${challenge}.h"
-
-END
-for funcName in ${funcNames}
-do
-   cat >> "${challenge}Funcs.cpp" << END
-extern ${funcSignature/_/${funcName}};
-END
-done
-cat >> "${challenge}Funcs.cpp" << END
-
-extern const int numAgents = ${numAgents};
-
-${funcSignature/_/(*agentFunc[numAgents])} = {
-END
-for funcName in ${funcNames}
-do
-   cat >> "${challenge}Funcs.cpp" << END
-   &${funcName},
-END
-done
-cat >> "${challenge}Funcs.cpp" << END
-};
-
-string agentStr[numAgents] = {
-END
-for funcName in ${funcNames}
-do
-   cat >> "${challenge}Funcs.cpp" << END
-   "${funcName#${challenge}Agent}",
-END
-done
-cat >> "${challenge}Funcs.cpp" << END
-};
-END
-
 echo " * ${challenge}.cpp: Compiling . . ."
 if ${CXX} ${CXXFLAGS} -c "${challenge}.cpp"
 then
@@ -106,18 +30,6 @@ else
    echo " * Build failed."
    rm *.o
    exit 3
-fi
-
-echo " * ${challenge}Funcs.cpp: Compiling . . ."
-if ${CXX} ${CXXFLAGS} -c "${challenge}Funcs.cpp"
-then
-   echo " * ${challenge}Funcs.cpp: Compilation succeeded."
-   rm -f "${challenge}Funcs.cpp"
-else
-   echo " * ${challenge}Funcs.cpp: Compilation failed."
-   echo " * Build failed."
-   rm *.o
-   exit 4
 fi
 
 echo " * ${challenge}Main.cpp: Compiling . . ."
@@ -145,7 +57,7 @@ fi
 echo " * Linking . . ."
 execFile="${challenge}RunSim"
 rm -f "${execFile}"
-if ${CXX} ${LDLIBS} -o "${execFile}" "${challenge}Main.o" "${challenge}Funcs.o" "${challenge}.o" "neuralNet.o" ${objectFiles}
+if ${CXX} ${LDLIBS} -o "${execFile}" "${challenge}Main.o" "${challenge}.o" "neuralNet.o" ${objectFiles}
 then
    echo " * Linking succeeded."
    echo " * Executable file ./${execFile} is ready to run!"
